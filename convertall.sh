44b9a463d30b8a177da645b0ceba21d87b76e7c8
Daniel Kearney <danielpatrickkearney@gmail.com>
2023-09-17 22:13:37 -0400

    Create convertall.sh

---
+++ convertall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+exiftool -r -d %s -tagsfromfile "%d/%F.json" "-GPSAltitude<GeoDataAltitude" "-GPSLatitude<GeoDataLatitude" "-GPSLatitudeRef<GeoDataLatitude" "-GPSLongitude<GeoDataLongitude" "-GPSLongitudeRef<GeoDataLongitude" "-Keywords<Tags" "-Subject<Tags" "-Caption-Abstract<Description" "-ImageDescription<Description" "-DateTimeOriginal<PhotoTakenTimeTimestamp" -ext "*" -overwrite_original -progress --ext json .
+
+mogrify -format jpg *.PNG
+mogrify -format jpg *.HEIC
+
+rm *.JSON
+rm *.HEIC
+rm *.PNG
+find . -type f  ! -name "*.*"  -delete
+
+exiv2 -r'%Y_%m_%d__%H_%M_%S' -F *.jpg
